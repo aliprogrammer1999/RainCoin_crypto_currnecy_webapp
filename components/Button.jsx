30e7a822b4b6764a7f9b04ca929807944f6774af
aliprogrammer1999 <ali.programer.1999@gmail.com>
2023-03-09 01:40:25 -0800

    modify buttn theme

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,56 +1,58 @@
 import React from "react";
 import { useTheme } from "next-themes";
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
-import { AiOutlineMenuFold, AiOutlineClose } from 'react-icons/ai'
 import { motion } from "framer-motion";
-
 
 export const ThemeBtn = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
   return (
-    <button
-      className="transition-all duration-200"
+    <motion.button
+      animate={{
+        rotate: theme === "dark" ? -180 : 0,
+      }}
       onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
     >
-      {theme === "dark" ? <BsFillMoonFill /> : <BsFillSunFill />}
-    </button>
+      {theme === "dark" ? (
+        <BsFillSunFill className="text-yellow-400" />
+      ) : (
+        <BsFillMoonFill className="text-black" />
+      )}
+    </motion.button>
   );
 };
 
+//navBet
 export const Navbtn = ({ toggle, navBtnEvent }) => {
-  const Path = props => (
-    <motion.path
-      strokeWidth="3"
-      stroke="white"
-
-      {...props}
-    />
+  const Path = (props) => (
+    <motion.path strokeWidth="4" stroke="white" {...props} />
   );
   return (
     <button
       onClick={toggle}
-      className={`${navBtnEvent ? "right-[285px] pl-[2px]" : "right-16"} bg-color-primery top-1/2 -translate-y-1/2 absolute z-[999] transition-all duration-1000 rounded-full w-8 h-8 pt-1 flex justify-center items-center`}
+      className={`${
+        navBtnEvent ? "right-[287px] pl-[2px]" : "right-16"
+      } bg-color-primery top-1/2 -translate-y-1/2 absolute z-[999] transition-all duration-1000 rounded-full w-7 h-7 pt-1 flex justify-center items-center`}
     >
-      <svg width="20" height="20" viewBox="0 0 23 23">
+      <svg width="17" height="17" viewBox="0 0 23 23">
         <Path
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
-            open: { d: "M 3 16.5 L 17 2.5" }
+            open: { d: "M 3 16.5 L 17 2.5" },
           }}
         />
         <Path
           d="M 2 9.423 L 20 9.423"
           variants={{
             closed: { opacity: 1 },
-            open: { opacity: 0 }
+            open: { opacity: 0 },
           }}
           transition={{ duration: 0.1 }}
         />
         <Path
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
-            open: { d: "M 3 2.5 L 17 16.346" }
+            open: { d: "M 3 2.5 L 17 16.346" },
           }}
         />
       </svg>
--- components/header/navbar/MobileNav.jsx
+++ components/header/navbar/MobileNav.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { motion, useCycle } from 'framer-motion'
-import { Navbtn } from '@/components/Button';
-import Link from 'next/link';
-import { GrBitcoin } from 'react-icons/gr'
+import React from "react";
+import { motion, useCycle } from "framer-motion";
+import { Navbtn } from "@/components/Button";
+import Link from "next/link";
+import { GrBitcoin } from "react-icons/gr";
 
 const navList = [
-  { title: "Home", link_list: '/' },
-  { title: "Exchange", link_list: '/exchange' },
-  { title: "Coin", link_list: '/coin' },
-  { title: "AboutUs", link_list: '/aboutus' },
-]
+  { title: "Home", link_list: "/" },
+  { title: "Exchange", link_list: "/exchange" },
+  { title: "Coin", link_list: "/coin" },
+  { title: "AboutUs", link_list: "/aboutus" },
+];
 
 const sidebar = {
   open: {
@@ -18,8 +18,8 @@ const sidebar = {
     transition: {
       type: "spring",
       stiffness: 40,
-      restDelta: 1
-    }
+      restDelta: 1,
+    },
   },
   closed: {
     width: 0,
@@ -27,38 +27,37 @@ const sidebar = {
     transition: {
       delay: 0,
       stiffness: 160,
-      damping: 100
-    }
-  }
+      damping: 100,
+    },
+  },
 };
 
 // motion Menu list
 const menu_Nav_motion = {
   open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
   },
   closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 }
-  }
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
 };
 
-// motion li 
+// motion li
 const List_item_motion = {
   open: {
     y: 0,
     opacity: 1,
     transition: {
-      y: { stiffness: 1000, velocity: -100 }
+      y: { stiffness: 1000, velocity: -100 },
     },
-
   },
   closed: {
     y: 50,
     opacity: 0,
     transition: {
-      y: { stiffness: 1000 }
-    }
-  }
+      y: { stiffness: 1000 },
+    },
+  },
 };
 
 const MobileNav = () => {
@@ -69,31 +68,62 @@ const MobileNav = () => {
     <motion.nav
       initial={false}
       animate={isOpen ? "open" : "closed"}
-      ref={containerRef}>
+      ref={containerRef}
+    >
+      <div
+        className={
+          isOpen
+            ? "fixed w-full top-0  right-0 m h-full bg-black/70"
+            : "bg-transparent"
+        }
+      >
+        <motion.div
+          className="w-[300px] z-0 fixed overflow-hidden flex flex-col justify-between py-12 items-center text-black top-0 right-0 h-full dark:bg-white bg-bg-main shadow-md dark:shadow-gray-300 shadow-gray-800"
+          variants={sidebar}
+        >
+          {/* navbar header logo  */}
+          <motion.div
+            variants={List_item_motion}
+            className="flex flex-col items-center"
+          >
+            <h1 className="flex items-center font-bold dark:text-black text-white ">
+              <span className="text-4xl text-color-primery">R</span>ain
+              <span className="text-4xl text-color-primery">C</span>oin
+              <GrBitcoin className="text-4xl" />
+            </h1>
+            <p className="text-xs mt-1 text-center dark:text-black text-white">
+              Lorem ipsum, unde ducimus id sapiente rerum incidunt voluptas
+              optio iure.
+            </p>
+          </motion.div>
 
-      <motion.div className="w-[300px] z-0 fixed overflow-hidden flex flex-col justify-between py-12 items-center text-black top-0 right-0 h-full dark:bg-white bg-bg-main shadow-md shadow-gray-300" variants={sidebar}>
-
-        {/* navbar header logo  */}
-        <motion.div variants={List_item_motion} className='flex flex-col items-center'>
-          <h1 className='flex items-center font-bold dark:text-black text-white '>
-            <span className='text-4xl text-color-primery'>R</span>ain<span className='text-4xl text-color-primery'>C</span>oin<GrBitcoin className='text-4xl' />
-          </h1>
-          <p className='text-xs mt-1 text-center dark:text-black text-white'>Lorem ipsum, unde ducimus id sapiente rerum incidunt voluptas optio iure.</p>
+          {/* navbar item */}
+          <motion.ul
+            variants={menu_Nav_motion}
+            className="w-full flex flex-col gap-3"
+          >
+            {navList.map((i, k) => (
+              <motion.li
+                key={k}
+                whileHover={{ scale: 1.05 }}
+                variants={List_item_motion}
+                className="w-full"
+              >
+                <Link
+                  href={i.link_list}
+                  className="dark:text-black text-white bg-[#242424] dark:bg-gray-300 block w-full p-2 rounded-md text-center"
+                >
+                  {i.title}
+                </Link>
+              </motion.li>
+            ))}
+          </motion.ul>
         </motion.div>
-
-        {/* navbar item */}
-        <motion.ul variants={menu_Nav_motion} className="w-full flex flex-col gap-3">
-          {navList.map((i, k) =>
-            <motion.li key={k} whileHover={{ scale: 1.05 }} variants={List_item_motion} className="w-full">
-              <Link href={i.link_list} className='dark:text-black text-white bg-[#242424] dark:bg-gray-300 block w-full p-2 rounded-md text-center'>{i.title}</Link>
-            </motion.li>
-          )}
-        </motion.ul>
-      </motion.div>
+      </div>
       {/* navbar Button  */}
       <Navbtn toggle={() => toggleOpen()} navBtnEvent={isOpen} />
-    </motion.nav >
-  )
-}
+    </motion.nav>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
